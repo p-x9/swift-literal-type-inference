0594cb659ca55c39a5b072b30a1785f412132609
p-x9 <50244599+p-x9@users.noreply.github.com>
2024-01-24 10:55:07 +0900

    format

--- Package.swift
+++ Package.swift
@@ -1,23 +1,24 @@
 // swift-tools-version: 5.9
-// The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "LiteralTypeInference",
     products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "LiteralTypeInference",
-            targets: ["LiteralTypeInference"]),
+            targets: ["LiteralTypeInference"]
+        ),
     ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "LiteralTypeInference"),
+            name: "LiteralTypeInference"
+        ),
         .testTarget(
             name: "LiteralTypeInferenceTests",
-            dependencies: ["LiteralTypeInference"]),
+            dependencies: [
+                "LiteralTypeInference"
+            ]
+        ),
     ]
 )
