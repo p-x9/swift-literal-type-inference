ecebe937680d24216cd0f2116263b13337883279
p-x9 <50244599+p-x9@users.noreply.github.com>
2024-08-27 23:36:23 +0900

    Adapt SwiftSyntax repository URL

--- Package.swift
+++ Package.swift
@@ -19,7 +19,7 @@ let package = Package(
     ],
     dependencies: [
         .package(
-            url: "https://github.com/apple/swift-syntax.git",
+            url: "https://github.com/swiftlang/swift-syntax.git",
             "509.0.0"..<"511.0.0"
         )
     ],
